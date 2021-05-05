d3bdf9ab0234cde07b9490c378dcbac7fabbf0d0
Lyudmil <workwith314@gmail.com>
2021-05-05 16:16:36 +0300

    Major store upate

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,24 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+$(document).ready(function(){
+  $('.slideshow-container').slick({
+    arrows: false
+  });
+  
+  $('.slideshow-container').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $(".dot").removeClass("active");
+    let e = $(".dot")[currentSlide];
+    $(e).addClass("active");
+  });
+});
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function nextSlide() {
+  $('.slideshow-container').slick("slickNext");
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function prevSlide() {
+  $('.slideshow-container').slick("slickPrev");
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function goToSlide(n) {
+  $('.slideshow-container').slick("slickGoTo", n);
 }
+
