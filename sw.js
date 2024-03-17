80fed5aa470b553f8153230e7716f1cef0d015e9
Lyudmil <workwith314@gmail.com>
2024-03-17 11:37:10 +0200

    sync with sw

--- sw.js
+++ sw.js
@@ -1,222 +1,134 @@
-// The version of the cache.
-const VERSION = "v1";
-
-// The name of the cache
-const CACHE_NAME = `JWAX-${VERSION}`;
-
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles/main.css',
-  '/scripts/main.js',
-  '/privacypolicy.html',
-  '/products.html',
-  '/savosin_double_skate_wax_&_cuffed_beanie.html',
-  '/savosin_single_skate_wax_&_skate_beanie.html',
-  '/shippingpolicy.html',
-  '/single_pack_skate_wax.html',
-  '/single_pack_skate_wax_savosin.html',
-  '/single_pack_snow_wax.html',
-  '/single_pack_surf_wax.html',
-  '/single_skate_wax_&_cuffed_beanie.html',
-  '/single_skate_wax_&_skate_beanie.html',
-  '/skate_caps.html',
-  '/skatewax-products.html',
-  '/ski&snowboardwax-products.html',
-  '/snow_single_wax_&_cuffed_beanie.html',
-  '/snow_single_wax_&_skate_beanie.html',
-  '/surfwax-products.html',
-  '/t-shirt.html',
-  '/team.html',
-  '/termsofuse.html',
-  '/winter_skate_beanies.html',
-  '/FAQ.html',
-  '/Returnandrefundpolicy.html',
-  '/_double_pack_skate_wax_savosin.html',
-  '/about.html',
-  '/accessories-products.html',
-  '/caps.html',
-  '/cdn-cgi/l/email-protection',
-  '/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js',
-  '/cdn-cgi/styles/cf.errors.css',
-  '/contact.html',
-  '/css/cookieconsent.css',
-  '/css/main.css',
-  '/cuffed_beanies.html',
-  '/deals-products.html',
-  '/double_pack_skate_wax.html',
-  '/double_pack_skate_wax_savosin.html',
-  '/double_pack_snow_wax.html',
-  '/double_pack_surf_wax.html',
-  '/double_skate_wax_&_cuffed_beanie.html',
-  '/double_skate_wax_&_skate_beanie.html',
-  '/giftcards-products.html',
-  '/giftcardspolicy.html',
-  '/img/JWAX-skate-wax.gif',
-  '/img/home-images/skate-wax.png',
-  '/img/home-images/snow-wax.png',
-  '/img/home-images/surf-wax.png',
-  '/img/jwax-team-bmx.gif',
-  '/img/jwax-team-rollerblading.gif',
-  '/img/jwax-team-scoot.gif',
-  '/img/jwax-team-skate.gif',
-  '/img/logo_b.svg',
-  '/img/products/caps/caps-1.png',
-  '/img/products/caps/caps-2.png',
-  '/img/products/caps/caps-3.png',
-  '/img/products/caps/caps-4.png',
-  '/img/products/caps/caps-5.png',
-  '/img/products/caps/caps-6.png',
-  '/img/products/caps/dad-caps-1.png',
-  '/img/products/caps/dad-caps-2.png',
-  '/img/products/caps/dad-caps-3.png',
-  '/img/products/caps/dad-caps-4.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-1.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-10.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-11.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-12.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-13.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-14.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-15.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-16.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-2.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-3.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-4.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-5.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-6.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-7.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-8.png',
-  '/img/products/cuffed-beanie/cuffed-beanie-9.png',
-  '/img/products/deals/double_skate_wax_&_cuffed_beanie_blue.png',
-  '/img/products/deals/double_skate_wax_&_cuffed_beanie_burgundyred.png',
-  '/img/products/deals/double_skate_wax_&_cuffed_beanie_classicred.png',
-  '/img/products/deals/double_skate_wax_&_cuffed_beanie_darkgray.png',
-  '/img/products/deals/double_skate_wax_&_cuffed_beanie_graphite.png',
-  '/img/products/deals/double_skate_wax_&_cuffed_beanie_lightgray.png',
-  '/img/products/deals/double_skate_wax_&_cuffed_beanie_orange.png',
-  '/img/products/deals/double_skate_wax_&_cuffed_beanie_pink.png',
-  '/img/products/deals/double_skate_wax_&_skate_beanie_dark.png',
-  '/img/products/deals/double_skate_wax_&_skate_beanie_light.png',
-  '/img/products/deals/savosin_double_skate_wax_&_cuffed_beanie_blue.png',
-  '/img/products/deals/savosin_double_skate_wax_&_cuffed_beanie_burgundyred.png',
-  '/img/products/deals/savosin_double_skate_wax_&_cuffed_beanie_classicred.png',
-  '/img/products/deals/savosin_double_skate_wax_&_cuffed_beanie_darkgray.png',
-  '/img/products/deals/savosin_double_skate_wax_&_cuffed_beanie_graphite.png',
-  '/img/products/deals/savosin_double_skate_wax_&_cuffed_beanie_lightgray.png',
-  '/img/products/deals/savosin_double_skate_wax_&_cuffed_beanie_orange.png',
-  '/img/products/deals/savosin_double_skate_wax_&_cuffed_beanie_pink.png',
-  '/img/products/deals/savosin_single_skate_wax_&_skate_beanie_dark.png',
-  '/img/products/deals/savosin_single_skate_wax_&_skate_beanie_light.png',
-  '/img/products/deals/single_skate_wax_&_cuffed_beanie_blue.png',
-  '/img/products/deals/single_skate_wax_&_cuffed_beanie_burgundyred.png',
-  '/img/products/deals/single_skate_wax_&_cuffed_beanie_classicred.png',
-  '/img/products/deals/single_skate_wax_&_cuffed_beanie_darkgray.png',
-  '/img/products/deals/single_skate_wax_&_cuffed_beanie_graphite.png',
-  '/img/products/deals/single_skate_wax_&_cuffed_beanie_lightgray.png',
-  '/img/products/deals/single_skate_wax_&_cuffed_beanie_orange.png',
-  '/img/products/deals/single_skate_wax_&_cuffed_beanie_pink.png',
-  '/img/products/deals/single_skate_wax_&_skate_beanie_dark.png',
-  '/img/products/deals/single_skate_wax_&_skate_beanie_light.png',
-  '/img/products/deals/snow_single_wax_&_cuffed_beanie_blue.png',
-  '/img/products/deals/snow_single_wax_&_cuffed_beanie_burgundyred.png',
-  '/img/products/deals/snow_single_wax_&_cuffed_beanie_classicred.png',
-  '/img/products/deals/snow_single_wax_&_cuffed_beanie_darkgray.png',
-  '/img/products/deals/snow_single_wax_&_cuffed_beanie_graphite.png',
-  '/img/products/deals/snow_single_wax_&_cuffed_beanie_lightgray.png',
-  '/img/products/deals/snow_single_wax_&_cuffed_beanie_orange.png',
-  '/img/products/deals/snow_single_wax_&_cuffed_beanie_pink.png',
-  '/img/products/deals/snow_single_wax_&_skate_beanie_dark.png',
-  '/img/products/deals/snow_single_wax_&_skate_beanie_light.png',
-  '/img/products/skate-wax/skate-wax-double-pack-wax.png',
-  '/img/products/skate-wax/skate-wax-double-pack.png',
-  '/img/products/skate-wax/skate-wax-savosin-pro-double-pack-img.png',
-  '/img/products/skate-wax/skate-wax-savosin-pro-double-pack-wax.png',
-  '/img/products/skate-wax/skate-wax-savosin-pro-double-pack.png',
-  '/img/products/skate-wax/skate-wax-savosin-pro-single-pack-img.png',
-  '/img/products/skate-wax/skate-wax-savosin-pro-single-pack-wax.png',
-  '/img/products/skate-wax/skate-wax-savosin-pro-single-pack.png',
-  '/img/products/skate-wax/skate-wax-single-pack-wax.png',
-  '/img/products/skate-wax/skate-wax-single-pack.png',
-  '/img/products/snow-wax/snow-wax-double-pack-wax.png',
-  '/img/products/snow-wax/snow-wax-double-pack.png',
-  '/img/products/snow-wax/snow-wax-single-pack-wax.png',
-  '/img/products/snow-wax/snow-wax-single-pack.png',
-  '/img/products/surf-wax/surf-wax-double-pack-wax.png',
-  '/img/products/surf-wax/surf-wax-double-pack.png',
-  '/img/products/surf-wax/surf-wax-single-pack-wax.png',
-  '/img/products/surf-wax/surf-wax-single-pack.png',
-  '/img/products/t-shirts/pattern-tshirt-detail.png',
-  '/img/products/t-shirts/pattern-tshirt.png',
-  '/img/products/thumbs/caps.png',
-  '/img/products/thumbs/cuffed-beanis.png',
-  '/img/products/thumbs/dad-caps.png',
-  '/img/products/thumbs/deals/double_skate_wax_&_cuffed_beanie_thumb.png',
-  '/img/products/thumbs/deals/double_skate_wax_&_skate_beanie_thumb.png',
-  '/img/products/thumbs/deals/savosin_double_skate_wax_&_cuffed_beanie_thumb.png',
-  '/img/products/thumbs/deals/savosin_single_skate_wax_&_skate_beanie_thumb.png',
-  '/img/products/thumbs/deals/single_skate_wax_&_cuffed_beanie_thumb.png',
-  '/img/products/thumbs/deals/single_skate_wax_&_skate_beanie_thumb.png',
-  '/img/products/thumbs/deals/snow_single_wax_&_cuffed_beanie_thumb.png',
-  '/img/products/thumbs/deals/snow_single_wax_&_skate_beanie_thumb.png',
-  '/img/products/thumbs/giftcards/giftcard_20.png',
-  '/img/products/thumbs/giftcards/giftcard_40.png',
-  '/img/products/thumbs/giftcards/giftcard_50.png',
-  '/img/products/thumbs/pattern-tshirt.png',
-  '/img/products/thumbs/skate-wax-double-pack.png',
-  '/img/products/thumbs/skate-wax-savosin-pro-double-pack.png',
-  '/img/products/thumbs/skate-wax-savosin-pro-single-pack.png',
-  '/img/products/thumbs/skate-wax-single-pack.png',
-  '/img/products/thumbs/snow-wax-double-pack.png',
-  '/img/products/thumbs/snow-wax-single-pack.png',
-  '/img/products/thumbs/surf-wax-double-pack.png',
-  '/img/products/thumbs/surf-wax-single-pack.png',
-  '/img/products/thumbs/winter-skate-beanies.png',
-  '/img/products/winter-skate-beanie/winter-skate-beanies-1.png',
-  '/img/products/winter-skate-beanie/winter-skate-beanies-2.png',
-  '/img/products/winter-skate-beanie/winter-skate-beanies-3.png',
-  '/img/products/winter-skate-beanie/winter-skate-beanies-4.png',
-  '/img/team/Alberto_Chakalov.png',
-  '/img/team/Atanas_Atanasov.png',
-  '/img/team/Evgeniy_Penkov.png',
-  '/img/team/Grigor_Grigorov.png',
-  '/img/team/Hristo_Iliev.png',
-  '/img/team/Ilia_Savosin.png',
-  '/img/team/Iliyan_Hriskov.png',
-  '/img/team/Kaloian_Trenkov.png',
-  '/img/team/Kate_Bedrata.jpg',
-  '/img/team/Kristian_Dimitrov.png',
-  '/img/team/Ralitsa_Chimshirova.png',
-  '/img/team/Rusen_Minchev.png',
-  '/img/team/Sorin_Coseraru.png',
-  '/img/team/Viktor_Pentchev.png',
-  '/js/collapsible-min.js',
-  '/js/cookieconsent-init-min.js',
-  '/js/cookieconsent-min.js',
-  '/js/form-min.js',
-  '/js/modal-min.js',
-  '/js/nav-min.js',
-  '/js/slick.min-min.js'
-  // Add other resources you want to cache here
+const version = 1;
+const cacheName = `thatsWhatSheSaid${version}`;
+const cacheList = [
+  './',
+  './index.html',
+  './about.html',
+  './ccs/main.css',
+  './404.html',
+  './404.png',
+  './main.js',
 ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
+self.addEventListener('install', (ev) => {
+  //load the cacheList array into the cache
+  ev.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      cache.addAll(cacheList);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        // No cache match, fetch from network
-        return fetch(event.request);
-      })
+self.addEventListener('activate', (ev) => {
+  //delete old versions of the cache
+  ev.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key != cacheName).map((nm) => caches.delete(nm))
+      );
+    })
   );
-}); 
+});
+
+self.addEventListener('fetch', (ev) => {
+  //handle fetch requests
+  //online? external? font? css? img? html? specific folder?
+  const isOnline = self.navigator.onLine;
+  const url = new URL(ev.request.url);
+  const isImage =
+    url.hostname.includes('picsum.photos') ||
+    url.pathname.includes('.png') ||
+    url.pathname.endsWith('.jpg');
+
+  const isJSON = url.hostname.includes('random-data-api.com');
+
+  const isCSS =
+    url.pathname.endsWith('.css') || url.hostname.includes('googleapis.com');
+  const isHTML = ev.request.mode === 'navigate';
+  const isFont =
+    url.hostname.includes('gstatic') || url.pathname.endsWith('woff2');
+
+  const selfUrl = new URL(self.location);
+  const isExternal =
+    ev.request.mode == 'cors' || selfUrl.hostname !== url.hostname;
+
+  if (isOnline) {
+    ev.respondWith(networkRevalidateAndCache(ev));
+  } else {
+    ev.respondWith(cacheOnly(ev));
+  }
+});
+
+function cacheOnly(ev) {
+  //only return what is in the cache
+  return caches.match(ev.request);
+}
+function cacheFirst(ev) {
+  //return from cache or fetch if not in cache
+  return caches.match(ev.request).then((cacheResponse) => {
+    //return cacheResponse if not null
+    return cacheResponse || fetch(ev.request);
+  });
+}
+function networkOnly(ev) {
+  //only return fetch response
+  return fetch(ev.request);
+}
+function networkFirst(ev) {
+  //try fetch and fallback on cache
+  return fetch(ev.request).then((fetchResponse) => {
+    if (fetchResponse.ok) return fetchResponse;
+    return caches.match(ev.request);
+  });
+}
+function staleWhileRevalidate(ev) {
+  //check cache and fallback on fetch for response
+  //always attempt to fetch a new copy and update the cache
+  return caches.match(ev.request).then((cacheResponse) => {
+    let fetchResponse = fetch(ev.request).then((response) => {
+      return caches.open(cacheName).then((cache) => {
+        cache.put(ev.request, response.clone());
+        return response;
+      });
+    });
+    return cacheResponse || fetchResponse;
+  });
+}
+function networkRevalidateAndCache(ev) {
+  //try fetch first and fallback on cache
+  //update cache if fetch was successful
+  return fetch(ev.request, { mode: 'cors', credentials: 'omit' }).then(
+    (fetchResponse) => {
+      if (fetchResponse.ok) {
+        //put in cache
+        return caches.open(cacheName).then((cache) => {
+          cache.put(ev.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      } else {
+        return caches.match(ev.request);
+      }
+    }
+  );
+}
+function placeholderImage(ev) {
+  //return a specific placeholder image from the cache
+  return caches.match('./404.png');
+}
+function Html404(ev) {
+  //return a 404 html file from the cache
+  return caches.match('./404.html');
+}
+function fakeServerError(ev) {
+  //pretend to have a server-side error
+  return new Response('<html><body><h1>Server Gone Crazy</h1></body></html>', {
+    status: 555,
+    statusText: 'Server Gone Crazy',
+    headers: {
+      'content-type': 'text/html',
+    },
+  });
+}
+function blahblahblah(ev) {
+  //invent your own strategy to deal with a specific circumstance in your app
+}
